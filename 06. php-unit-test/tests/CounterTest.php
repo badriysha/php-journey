01eca984d8d0cb8768b47a0f2d22e87434f166be
badri yusron <badriysha@gmail.com>
2022-01-10 21:25:36 +0700

    day 9: learn fixer tearDown and @after

--- 06. php-unit-test/tests/CounterTest.php
+++ 06. php-unit-test/tests/CounterTest.php
@@ -6,35 +6,40 @@ use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
 class CounterTest extends TestCase {
+    private Counter $counter;
+
+    protected function setUp(): void
+    {
+        $this->counter = new Counter();
+        echo "Set up Counter" . PHP_EOL;
+    }
+
     public function testCounter() {
-        $counter = new Counter();
         // cara 1
-        $counter->increment();
-        Assert::assertEquals(1, $counter->getCounter());
+        $this->counter->increment();
+        Assert::assertEquals(1, $this->counter->getCounter());
         // cara 2
-        $counter->increment();
-        $this->assertEquals(2, $counter->getCounter());
+        $this->counter->increment();
+        $this->assertEquals(2, $this->counter->getCounter());
         // cara 3
-        $counter->increment();
-        self::assertEquals(3, $counter->getCounter());
+        $this->counter->increment();
+        self::assertEquals(3, $this->counter->getCounter());
     }
 
     /**
      * @test
      */
     public function increment() {
-        $counter = new Counter();
-        $counter->increment();
-        Assert::assertEquals(1, $counter->getCounter());
+        $this->counter->increment();
+        Assert::assertEquals(1, $this->counter->getCounter());
     }
 
     public function testFirst() :Counter
     {
-        $counter = new Counter();
-        $counter->increment();
-        $this->assertEquals(1, $counter->getCounter());
+        $this->counter->increment();
+        $this->assertEquals(1, $this->counter->getCounter());
 
-        return $counter;
+        return $this->counter;
     }
 
     /**
@@ -44,5 +49,18 @@ class CounterTest extends TestCase {
     {
         $counter->increment();
         $this->assertEquals(2, $counter->getCounter());
+    }
+
+    protected function tearDown(): void
+    {
+        echo "Tear Down" . PHP_EOL;
+    }
+
+    /**
+     * @after
+     */
+    public static function tearDownAfterClass(): void
+    {
+        echo "after" . PHP_EOL;
     }
 }
