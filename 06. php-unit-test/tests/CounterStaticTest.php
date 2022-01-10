4ed560053ead8f838b7403b77861a76a17903234
badri yusron <badriysha@gmail.com>
2022-01-10 22:14:11 +0700

    day 9: learn Fixture sharing beforeClass & afterClass

---
+++ 06. php-unit-test/tests/CounterStaticTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Badriysha\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class CounterStaticTest extends TestCase
+{
+    private static Counter $counter;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$counter = new Counter();
+    }
+
+    public function testFirst()
+    {
+        self::$counter->increment();
+        self::assertEquals(1, self::$counter->getCounter());
+    }
+
+    public function testSecond()
+    {
+        self::$counter->increment();
+        self::assertEquals(2, self::$counter->getCounter());
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        echo "Test finished" . PHP_EOL;
+    }
+
+}
